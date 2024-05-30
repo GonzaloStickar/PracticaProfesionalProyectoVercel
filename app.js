2ac2085bb7da9a3c3b224af3485aca879cd2cd65
Gonzalo <gonzaloostickarr@gmail.com>
2024-05-30 17:58:14 -0300

    prueba html

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require("express"); 
-const app = express(); 
+const express = require("express");
+const app = express();
+const { main } = require('./api/main');
 
 // A simple get greet method 
 app.get("/", (req, res) => { 
-    // get the passed query 
-    const { name } = req.query; 
-    res.send({ msg: `aaaaaaaaaaaaaa ${name}!` }); 
-}); 
+    res.redirect('/inicio'); 
+});
+
+app.get('/inicio', main);
 
 // export the app for vercel serverless functions 
 module.exports = app;
