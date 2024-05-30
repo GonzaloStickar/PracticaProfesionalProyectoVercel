9052ee1127e43b64654320bdc0866f164fbe8a1a
Gonzalo <gonzaloostickarr@gmail.com>
2024-05-30 18:58:31 -0300

    conexion fin

--- api/main.js
+++ api/main.js
@@ -1,11 +1,10 @@
 const path = require('path');
 
 const main = (req, res) => {
-    // Ruta al archivo index.html (directamente)
-    const indexPath = path.join(process.cwd(), 'public', 'templates', 'index.html');
-    console.log("Ruta al archivo index.html:", indexPath);
 
-    // Enviar el archivo HTML como respuesta
+    // Obligatorio usar 'process.cdw()' si usamos Vercel
+    const indexPath = path.join(process.cwd(), 'public', 'templates', 'index.html');
+
     res.sendFile(indexPath, (err) => {
         if (err) {
             console.error('Error al enviar el archivo:', err);
--- app.js
+++ app.js
@@ -6,24 +6,15 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true })); // Necesario para recibir datos de formularios (Middleware)
 app.listen()
 
-//app.get("/inicio", (req, res) => {
-//    res.send("Esta es la página de inicio");
-//});
-
 app.get("/inicio", main)
 
 app.get("/", (req, res) => { 
-    //res.redirect('/inicio');
-    res.send({"hola":"hola"})
+    res.redirect('/inicio');
 });
-
-
 
 app.get("*", (req, res) => {
     res.status(404).send("Página no encontrada");
 });
-
-
 
 // export the app for vercel serverless functions
 module.exports = app;
