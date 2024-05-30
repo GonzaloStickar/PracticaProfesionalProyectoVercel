43e6eab70f214384d8387f1d2fd1769ae8d40d31
Gonzalo <gonzaloostickarr@gmail.com>
2024-05-30 18:11:23 -0300

    prueba fix html

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const { main } = require('./api/main');
 
 // A simple get greet method 
 app.get("/", (req, res) => { 
-    res.redirect('/inicio'); 
+    res.redirect('/inicio');
 });
 
-app.get('/inicio', main);
+//app.get('/inicio', main);
 
 // export the app for vercel serverless functions 
 module.exports = app;
