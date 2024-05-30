e2a4ee5d6661039c242a0633c5ebe4bc18b337c1
Gonzalo <gonzaloostickarr@gmail.com>
2024-05-30 17:31:36 -0300

    fix

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express"); 
-const app = express();
-const { mainPage } = require("./api/main")
+const app = express(); 
 
+// A simple get greet method 
 app.get("/", (req, res) => { 
-    res.redirect('/inicio');
-});
-
-app.get('/inicio', mainPage);
+    // get the passed query 
+    const { name } = req.query; 
+    res.send({ msg: `aaaaaaaaaaaaaa ${name}!` }); 
+}); 
 
 // export the app for vercel serverless functions 
 module.exports = app;
